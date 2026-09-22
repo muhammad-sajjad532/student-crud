58c421cfacb42b0fa09a486290e75ac1123e3246
Muhammad Sajjad <muhammad.sajjad@axabiztech.com>
2026-09-22 15:04:19 +0500

    testing

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -12,7 +12,7 @@ export class StudentsComponent {
   readonly students = signal<Student[]>([
     { id: 1, name: 'Ali' },
     { id: 2, name: 'Sara' },
-    { id: 3, name: 'John' },
+    //{ id: 3, name: 'John' },
   ]);
 
   name = '';
